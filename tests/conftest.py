e88c23e29fbddfe37aa2ab5b82e9e77bc91011ae
Sigmond <12134189a@gmail.com>
2026-01-11 19:35:05 +0200

    Delete tests/conftest.py

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-
-def pytest_configure() -> None:
-    repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    src_path = os.path.join(repo_root, "src")
-    if src_path not in sys.path:
-        sys.path.insert(0, src_path)
